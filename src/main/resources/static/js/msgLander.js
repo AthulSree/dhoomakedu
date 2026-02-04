eb4e030f937d93ff8f7db23c523b4bb20fa9ee5a
name <mail>
2026-02-04 20:53:50 +0530

    This version of Antigravity is no longer supported. Please update to receive the latest features!

--- src/main/resources/static/js/msgLander.js
+++ src/main/resources/static/js/msgLander.js
@@ -1,4 +1,22 @@
 
 $(function(){
     $("#hostdd").select2();
+
+//MPR grp selection
+    $(document).on("click", "#grp_dd_mpr", function(){
+        $('#hostdd').val(['1', '3', '4', '5', '6']).trigger('change');
+    })
+
+//Tea grp selection
+    $(document).on("click", "#grp_dd_tea", function(){
+        $('#hostdd').val(['1', '3', '5', '6']).trigger('change');
+    })
+
+//Crex grp selection
+    $(document).on("click", "#grp_dd_crex", function(){
+        $('#hostdd').val(['3']).trigger('change');
+    })
+
+
+
 })
